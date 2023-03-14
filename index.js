e8e09497835aac7b372c28dd7a9044527a58cb1e
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-03-14 12:43:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400){
+    result = 'This one is on me!';
+  }
+
+  else if (feet > 2500){
+    result = 'No can do.';
+  }
+  else {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case'generous':
+      return'Thank you so much.';
+    case'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
+
+
+
+
